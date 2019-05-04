3680628cb0c3b907ea5c4f8d3f0bb19af3db82b2
Elena Eliseeva <elenazamorskayaselenium@gmail.com>
2019-05-05 00:43:50 +0300

    Initial commit

---
+++ src/main/java/qalight/selenium36/App.java
@@ -0,0 +1,13 @@
+package qalight.selenium36;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
